27856d88f2f8fdc22725aeb4b5c3c5448e5db86e
shivaprasad-creatar <shivakondagadupula@gmail.com>
2026-01-15 09:02:45 +0530

    Add AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is an online plant shop that offers a wide range of
+        indoor and outdoor plants. Our mission is to provide healthy plants
+        and make plant shopping easy and enjoyable for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
+
